da941bf3847524517cee1fa946bdaced8bad1787
jeremybeaugeard <49061410+jeremybeaugeard@users.noreply.github.com>
2019-06-25 09:23:47 +0200

    Update MainScan.cpp
    Changement des arguments du main

--- Back-End/Controller/Scan-Analyses/MainScan.cpp
+++ Back-End/Controller/Scan-Analyses/MainScan.cpp
@@ -1,46 +1,32 @@
-
 #include <iostream>
 #include <QApplication>
 #include <QGraphicsScene>
 #include <QtWidgets/QGraphicsView>
 #include <QtWidgets/QGraphicsPixmapItem>
 #include <QDebug>
+#include <QCoreApplication>
+#include <QtWidgets/qapplication.h>
+
 #include "Scanner.h"
 #include "MainScan.h"
+#include "../DownloadManager.h"
 
-vector<pair <int,int>> MainScan(int argc, char** argv, int id_student, int id_promotion){
+void MainScan(int argc, char** argv, int id_student, int id_promotion, vector<pair <int,int>> &answers, Qstring &stringImage){
     QApplication a(argc, argv);
     QGraphicsScene scene;
     QGraphicsView view(&scene);
-    //QPixmap image("img/1/5.jpg");
-    QPixmap image("img/hautGauche.jpg");
-    //QPixmap image("img/basGauche.jpg");
-    //QPixmap image("img/hautDroite.jpg");
-    //QPixmap image("img/basDroite.jpg");
+
+    DownloadManager manager;
+    manager.doDownload(idPromotion, idStudent); // téléchargement de l'image
+
+    QPixmap image("../DownloadManager/student_answer.jpg");
 
     QImage im = image.toImage().convertToFormat(QImage::Format_ARGB32);
     QImage imageCreate(image.width(), image.height(), QImage::Format(QImage::Format_ARGB32));
 
-    // phase de test
     Scanner scan(im);
-    //vector<int> posx = scan.oppositePositions(true);
-    vector<pair <pair <int,int>, pair <int,int>>> posss = scan.boxPositions();
-    vector<pair <int,int>> pos = scan.getAnswers(posss);
-
-    // debug
-
-    /*for (int i = 0; i < posx.size(); i++){
-        //scan.debugImage(scan.getStartX(), posx[i]);
-        //scan.debugImage(posx[i], scan.getStartY());
-
-        //scan.debugImage(posx[i], im.height() - scan.getStartY());
-        //scan.debugImage( im.width() - scan.getStartX(), posx[i]);
-        cout << "x: " << im.height() - scan.getStartY() << ", Y : " << posx[i] << endl;
-    }
-    cout << "startx: " << scan.getStartX() << ", startY : " << scan.getStartY() << endl;
-    cout << "maxwidth: " << im.width() << ", maxheight : " << im.height() << endl;
-    cout << posx.size() << endl;*/
-
+    scan.getAnswers(&answers);
+    scan.im
     QPixmap newImage = QPixmap::fromImage(scan.getImage());
     // display
     QGraphicsPixmapItem item(newImage);
